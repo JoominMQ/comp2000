f420dbe267197a1063671698a96c69e8dc8b08c2
mg1273 <georgecanten21@gmail.com>
2026-08-17 14:15:54 +1000

    a magenta square

--- project/Simulation/src/App.java
+++ project/Simulation/src/App.java
@@ -1,17 +1,23 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
-
-public class App{
+public class App {
     public static void main(String[] args) throws Exception {
         JFrame frame = new JFrame("Test stringing nows");
-        JPanel panel = new JPanel(){
+
+        JPanel panel = new JPanel() {
             @Override
-            protected void paintComponent(Graphics g){
+            protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
+
                 g.setColor(Color.MAGENTA);
                 g.fillRect(50, 50, 100, 100);
             }
         };
+
+        frame.add(panel);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
