9388050eadb6b53f5fe2f700233c403be27e8c0c
Alexsolmariansalamander <alexander.vissarion@students.mq.edu.au>
2026-08-17 14:11:26 +1000

    Update App.java
    onga

--- project/Simulation/src/App.java
+++ project/Simulation/src/App.java
@@ -1,5 +1,17 @@
-public class App {
+import javax.swing.*;
+import java.awt.*;
+
+
+public class App{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("Test stringing nows");
+        JPanel panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                g.setColor(Color.MAGENTA);
+                g.fillRect(50, 50, 100, 100);
+            }
+        };
     }
 }
